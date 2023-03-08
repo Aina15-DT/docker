b807530d30515d64710243b724e37bb1a5083f15
Yves.HaiRun <yvessilvere@hairun-technology.com>
2023-03-08 17:58:36 +0300

    stage 2

--- app/main.py
+++ app/main.py
@@ -7,8 +7,9 @@ def main():
     command = sys.argv[3]
     args = sys.argv[4:]
     
-    completed_process = subprocess.run([command, *args], capture_output=True)
-    print(completed_process.stdout.decode("utf-8"))
+    completed_process = subprocess.run(
+        [command, *args], capture_output=False, stdout=sys.stdout, stderr=sys.stderr
+    )
 
 
 if __name__ == "__main__":
